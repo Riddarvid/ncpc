48734e55689885f91210e9704490f024f31f986c
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2022-10-06 22:17:42 +0200

    Add ncpc java template

---
+++ src/ncpc/NcpcTemplate.java
@@ -0,0 +1,34 @@
+package ncpc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class NcpcTemplate {
+    public static void main(String[] args) throws FileNotFoundException {
+        new NcpcTemplate().run();
+    }
+
+    private void run() throws FileNotFoundException {
+        parseInput();
+        solve();
+        printSolution();
+    }
+
+    private void parseInput() throws FileNotFoundException {
+        String filePath = "ncpc/ncpc2022/practice1/adolescentarchitecture/input1";
+        File inputFile = new File(filePath);
+        Scanner scanner = new Scanner(inputFile);
+
+        //TODO Comment out before submitting!!!
+        //Scanner scanner = new Scanner(System.in);
+    }
+
+    private void solve() {
+
+    }
+
+    private void printSolution() {
+
+    }
+}
--- src/ncpc/ncpc2022/practice1/adolescentarchitecture/AdolescentArchitecture.java
+++ src/ncpc/ncpc2022/practice1/adolescentarchitecture/AdolescentArchitecture.java
@@ -1,4 +1,6 @@
 package ncpc.ncpc2022.practice1.adolescentarchitecture;
 
 public class AdolescentArchitecture {
+    public static void main(String[] args) {
+    }
 }
